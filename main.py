71589aa230884bf6a356f104eee882fac75fba5b
Andrey Maltesv <andrey.malcev@pimpay.ru>
2021-09-15 17:13:47 +0300

    GET запрос на длинный редирект.

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import requests
 
-response = requests.get("https://playground.learnqa.ru/api/get_text")
-print(response.text)
+response = requests.get("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
+first_response = response.history[0]
+second_response = response.history[1]
+finally_response = response.history[2]
+
+print(first_response.url)
+print(second_response.url)
+print("Конечный URL", finally_response.url)

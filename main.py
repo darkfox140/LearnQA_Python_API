de899cad34caad1ddd214ef584263484a4f3af55
Andrey Maltesv <andrey.malcev@pimpay.ru>
2021-09-21 14:05:21 +0300

    Ex7: Запросы и методы 1. Запрос без метода выведет текст "Wrong method provided" 2. Запрос с методом не из списка ничего не выводит 3. Запрос с правильным методов выводит "{"success":"!"}" 4. Метод "DELETE" с параметром "GET" выдаёт "{"success":"!"}" - Баг(ожидаемый результат "Respose: code 200, text "Wrong method provided"), методы "PATCH" и "OPTIONS" выдают 400 ошибку но при этом выдают текст "Wrong HTTP method" - Баг(ожидаемый результат Respose: code 400)

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 import requests
 
-response = requests.get("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
-first_response = response.history[0]
-second_response = response.history[1]
-finally_response = response.history[2]
 
-print(first_response.url)
-print(second_response.url)
-print("Конечный URL", finally_response.url)
+def check_http_methods():
+
+    methods_list = ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "COPY", "HEAD"]
+    link = "https://playground.learnqa.ru/ajax/api/compare_query_type"
+    allowed_methods = ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"]
+    print_string = "HTTP type: {}, method in params/data: {}\nRespose: code {}, text \"{}\"\nКоректно: {}\n"
+
+    for http_method in allowed_methods:
+        for check_method in methods_list:
+            if http_method == "GET":
+                result = requests.request(method=http_method,
+                                          url=link,
+                                          params={"method": check_method})
+            else:
+                result = requests.request(method=http_method,
+                                          url=link,
+                                          data={"method": check_method})
+
+            print(print_string.format(result.request.method,
+                                      check_method,
+                                      result.status_code,
+                                      result.text,
+                                      result.text == '{"success":"!"}'))
+
+
+check_http_methods()

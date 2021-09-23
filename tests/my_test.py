a38b78095859b6e34c7dd1accc22b697e937d932
Andrey Maltesv <andrey.malcev@pimpay.ru>
2021-09-23 17:16:18 +0300

    Ex11: Тест запроса на метод cookie

--- tests/my_test.py
+++ tests/my_test.py
@@ -1,10 +1,9 @@
-import random
-import string
+import requests
 
 
 class TestShortPhraseTest:
     def test_check_phrase(self):
-        phrase = input("Set a phrase: ")
-        symbols = "".join([random.choice(string.ascii_letters) for e in range(random.randrange(15, 16))])
-
-        assert len(phrase) < len(symbols), f"Строка {phrase}, либо больше либо равна {symbols}"
+        url = "https://playground.learnqa.ru/api/homework_cookie"
+        response = requests.get(url)
+        cookies = response.cookies
+        assert cookies == {'HomeWork': 'hw_value'}

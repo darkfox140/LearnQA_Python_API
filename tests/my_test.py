1f3e3607b9a748958c8c10adc5d2999de7ec0a03
Andrey Maltesv <andrey.malcev@pimpay.ru>
2021-09-24 11:29:09 +0300

    Ex11: Тест запроса на метод cookie

--- tests/my_test.py
+++ tests/my_test.py
@@ -1,16 +1,16 @@
 import requests
-import json
 
 
-class TestShortPhraseTest:
-    def test_check_phrase(self):
-        url = "https://playground.learnqa.ru/api/homework_header"
-
+class TestCookies:
+    def test_check_cookies(self):
+        url = "https://playground.learnqa.ru/api/homework_cookie"
         response = requests.get(url)
+        cookies = response.cookies
+        print(dict(cookies))
+
         assert response.status_code == 200, "Wrong response code"
+        assert "HomeWork" in cookies, "В cookies нет поля 'HomeWork'"
 
-        actual_response_text = response.headers['x-secret-homework-header']
-        expected_response_text = 'Some secret value'
-
-        assert 'x-secret-homework-header' in response.headers
-        assert expected_response_text == actual_response_text
+        expected_response_cookies_value = 'hw_value'
+        actual_response_cookies_value = cookies['HomeWork']
+        assert expected_response_cookies_value == actual_response_cookies_value, "Значения cookies не совпадают"

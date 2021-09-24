452b694968225e5391b3de11d781ad6a2046c49d
Andrey Maltesv <andrey.malcev@pimpay.ru>
2021-09-24 11:56:52 +0300

    Ex12: Тест запроса на метод header

--- tests/my_test.py
+++ tests/my_test.py
@@ -1,16 +1,16 @@
 import requests
 
 
-class TestCookies:
-    def test_check_cookies(self):
-        url = "https://playground.learnqa.ru/api/homework_cookie"
+class TestHeader:
+    def test_check_header(self):
+        url = "https://playground.learnqa.ru/api/homework_header"
         response = requests.get(url)
-        cookies = response.cookies
-        print(dict(cookies))
+        headers = response.headers
+        print(headers)
 
         assert response.status_code == 200, "Wrong response code"
-        assert "HomeWork" in cookies, "В cookies нет поля 'HomeWork'"
+        assert "x-secret-homework-header" in headers, "В headers нет поля 'x-secret-homework-header'"
 
-        expected_response_cookies_value = 'hw_value'
-        actual_response_cookies_value = cookies['HomeWork']
-        assert expected_response_cookies_value == actual_response_cookies_value, "Значения cookies не совпадают"
+        expected_response_headers_value = 'Some secret value'
+        actual_response_headers_value = headers['x-secret-homework-header']
+        assert expected_response_headers_value == actual_response_headers_value, "Значения headers не совпадают"

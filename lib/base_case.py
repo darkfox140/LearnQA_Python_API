86a40afbbdf29dd1ef4b67c28931104a7a9b3748
Andrey Maltesv <andrey.malcev@pimpay.ru>
2021-09-30 13:39:47 +0300

    Добавлены новые методы

--- lib/base_case.py
+++ lib/base_case.py
@@ -1,23 +1,38 @@
-import json.decoder
 from requests import Response
+import json.decoder
+from datetime import datetime
 
 
 class BaseCase:
 
     def get_cookie(self, response: Response, cookie_name):
-        assert cookie_name in response.cookies, f"Cannot find cookie with name {cookie_name} in the last response"
+        assert cookie_name in response.cookies, f"Не удается найти файл cookie с именем {cookie_name} в последнем ответе"
         return response.cookies[cookie_name]
 
     def get_header(self, response: Response, headers_name):
-        assert headers_name in response.headers, f"Cannot find header with name {headers_name} in the last response"
+        assert headers_name in response.headers, f"Не удается найти header с именем {headers_name} в последнем ответе"
         return response.headers[headers_name]
 
     def get_json_value(self, response: Response, name):
         try:
             response_as_dict = response.json()
         except json.decoder.JSONDecodeError:
-            assert False, f"Response is not JSON Format. Response text is '{response.text}'"
+            assert False, f"Ответ не в формате JSON. Текст ответа '{response.text}'"
 
-        assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
+        assert name in response_as_dict, f"В ответе JSON нет ключа '{name}'"
 
         return response_as_dict[name]
+
+    def prepare_registration_data(self, email=None):
+        if email is None:
+            base_part = "learnqa"
+            domain = "example.com"
+            random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+            email = f"{base_part}{random_part}@{domain}"
+        return {
+            'password': '123',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': email,
+        }

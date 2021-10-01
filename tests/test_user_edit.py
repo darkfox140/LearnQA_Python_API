70d79b52174a930f85af96189dcb3a0978d16bb7
Andrey Maltesv <andrey.malcev@pimpay.ru>
2021-10-01 12:30:10 +0300

    Изменения в тестах с помощью новых библиотек

--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -1,6 +1,6 @@
-import requests
 from lib.base_case import BaseCase
 from lib.assertios import Assertions
+from lib.my_requests import MyRequests
 
 
 class TestUserEdit(BaseCase):
@@ -8,7 +8,7 @@ class TestUserEdit(BaseCase):
     def test_edit_just_created_user(self):
         # Регистрация
         register_data = self.prepare_registration_data()
-        response1 = requests.post("https://playground.learnqa.ru/api/user", data=register_data)
+        response1 = MyRequests.post("/user", data=register_data)
 
         Assertions.assert_status_code(response1, 200)
         Assertions.assert_json_has_key(response1, "id")
@@ -24,16 +24,16 @@ class TestUserEdit(BaseCase):
             'password': password,
         }
 
-        response2 = requests.post("https://playground.learnqa.ru/api/user/login", data=login_data)
+        response2 = MyRequests.post("/user/login", data=login_data)
 
         auth_sid = self.get_cookie(response2, "auth_sid")
         token = self.get_header(response2, "x-csrf-token")
 
         # Редактирование
-        new_name = "Change Name Paty"
+        new_name = "Change Name Party"
 
-        response3 = requests.put(
-            f"https://playground.learnqa.ru/api/user/{user_id}",
+        response3 = MyRequests.put(
+            f"/user/{user_id}",
             headers={"x-csrf-token": token},
             cookies={"auth_sid": auth_sid},
             data={"firstName": new_name}
@@ -42,8 +42,8 @@ class TestUserEdit(BaseCase):
         Assertions.assert_status_code(response3, 200)
 
         # GET
-        response4 = requests.get(
-            f"https://playground.learnqa.ru/api/user/{user_id}",
+        response4 = MyRequests.get(
+            f"/user/{user_id}",
             headers={"x-csrf-token": token},
             cookies={"auth_sid": auth_sid},
         )

fc895752707d9dcf9abcc0188be78bfe04eba2c6
Andrey Maltesv <andrey.malcev@pimpay.ru>
2021-10-05 13:08:55 +0300

    ФEx15: Тесты на метод user

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,12 +1,21 @@
 from lib.assertios import Assertions
 from lib.base_case import BaseCase
 from lib.my_requests import MyRequests
+import pytest
 
 
 class TestUserRegister(BaseCase):
 
+    test_params = [
+        ({'password': 'qwerty', 'username': 'qwerty', 'firstName': 'qwerty', 'lastName': 'qwerty'}, 'email'),
+        ({'username': 'qwerty', 'firstName': 'qwerty', 'lastName': 'qwerty', 'email': 'test@example.com'}, 'password'),
+        ({'password': 'qwerty', 'firstName': 'qwerty', 'lastName': 'qwerty', 'email': 'test@example.com'}, 'username'),
+        ({'password': 'qwerty', 'username': 'qwerty', 'lastName': 'qwerty', 'email': 'test@example.com'}, 'firstName'),
+        ({'password': 'qwerty', 'username': 'qwert', 'firstName': 'qwerty', 'email': 'test@example.com'}, 'lastName')
+    ]
+
     def test_create_user_successfully(self):
-        data = self.prepare_registration_data()
+        data = self.prepare_registration_data(first_name='firstName')
 
         response = MyRequests.post('/user/', data=data)
 
@@ -32,8 +41,18 @@ class TestUserRegister(BaseCase):
         Assertions.assert_status_code(response, 400)
         assert response.content.decode("utf-8") == "Invalid email format", f"Не ожидаемый ответ: {response.content}"
 
+    @pytest.mark.parametrize('test', test_params)
+    def test_creating_user_121(self, test):
+        data, empty_key = test
+
+        response = MyRequests.post('/user/', data=data)
+
+        Assertions.assert_status_code(response, 400)
+        assert response.content.decode("utf-8") == f"The following required params are missed: {empty_key}", \
+            f"Не ожидаемый ответ: {response.conten}"
+
     def test_creating_user_with_a_short_name(self):
-        firstName = self.random_string()[1]
+        firstName = self.random_string(1)
         data = self.prepare_registration_data(first_name=firstName)
 
         response = MyRequests.post('/user/', data=data)
@@ -43,8 +62,8 @@ class TestUserRegister(BaseCase):
             f"Не ожидаемый ответ: {response.conten}"
 
     def test_creating_user_with_a_long_name(self):
-        firstName = self.random_string()
-        data = self.prepare_registration_data(first_name=firstName)
+        first_name = self.random_string(251)
+        data = self.prepare_registration_data(first_name=first_name)
 
         response = MyRequests.post('/user/', data=data)
 

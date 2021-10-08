b7b4f3b8e1d903b1f68ddfa4dbd9b44fcdaf9b82
Andrey Maltesv <andrey.malcev@pimpay.ru>
2021-10-08 13:57:50 +0300

    Ex18: Тесты на DELETE
    Попробовать удалить пользователя, будучи авторизованными други пользователем есть явный баг, что при попытке удалить пользователя по ID будучи авторизированным другим пользователем отдаётся код статуса 200 хотя явно ожидаем 400 статус

--- tests/test_user_delete.py
+++ tests/test_user_delete.py
@@ -6,7 +6,7 @@ import time
 
 class TestUserRegister(BaseCase):
 
-    '''def test_delete_user_by_id_2(self):
+    def test_delete_user_by_id_2(self):
         data = {
             'email': 'vinkotov@example.com',
             'password': '1234'
@@ -60,58 +60,45 @@ class TestUserRegister(BaseCase):
         response4 = MyRequests.get(f"/user/{user_id}", headers={"x-csrf-token": token}, cookies={"auth_sid": auth_sid})
 
         Assertions.assert_status_code(response4, 404)
-        Assertions.assert_text(response4, "User not found")'''
+        Assertions.assert_text(response4, "User not found")
 
-    def test_deleting_a_user_by_id_by_others_users(self):
-        data_register1 = self.prepare_registration_data()
-        time.sleep(2)
-        data_register2 = self.prepare_registration_data()
+    def test_deleting_a_user_by_id_by_others_users1111(self):
+        register_data_1 = self.prepare_registration_data()
 
-        response1 = MyRequests.post("/user/", data=data_register1)
+        response_1 = MyRequests.post("/user/", data=register_data_1)
+        Assertions.assert_status_code(response_1, 200)
 
-        Assertions.assert_status_code(response1, 200)
+        user_id1 = self.get_json_value(response_1, 'id')
 
-        email1 = data_register1['email']
-        password1 = data_register1['password']
-        user_name1 = data_register1['username']
-        user_id1 = self.get_json_value(response, 'id')
+        time.sleep(1)
 
-        response2 = MyRequests.post("/user/", data=data_register2)
+        register_data_2 = self.prepare_registration_data()
 
+        response2 = MyRequests.post("/user/", data=register_data_2)
         Assertions.assert_status_code(response2, 200)
 
-        email2 = data_register2['email']
-        password2 = data_register2['password']
-        user_name2 = data_register2['username']
+        email = register_data_2['email']
+        password = register_data_2['password']
         user_id2 = self.get_json_value(response2, 'id')
 
-        new_login_data1 = {
-            "email": email1,
-            "password": password1,
+
+        login_data2 = {
+            "email": email,
+            "password": password,
         }
 
-        response3 = MyRequests.get("/user/login", data=new_login_data1)
-
-        Assertions.assert_status_code(response3, 200)
-
-        auth_sid1 = self.get_cookie(response3, "auth_sid")
-        token1 = self.get_header(response3, "x-csrf-token")
-
-        new_login_data2 = {
-            "email": email2,
-            "password": password2,
-        }
-
-        response4 = MyRequests.post("/user/login", data=new_login_data2)
-
+        response4 = MyRequests.post("/user/login", data=login_data2)
         Assertions.assert_status_code(response4, 200)
 
-        auth_sid2 = self.get_cookie(response3, "auth_sid")
-        token2 = self.get_header(response3, "x-csrf-token")
+        auth_sid = self.get_cookie(response4, "auth_sid")
+        token = self.get_header(response4, "x-csrf-token")
 
-        response5 = MyRequests.get(f"/user/{user_id1}",
-                                      headers={"x-csrf-token": token1},
-                                      cookies={"auth_sid": auth_sid1})
+        response5 = MyRequests.delete(f"/user/{user_id1}",
+                                      headers={"x-csrf-token": token},
+                                      cookies={"auth_sid": auth_sid})
 
-        print(response5.content)
-        Assertions.assert_status_code(response5, 200)
+        Assertions.assert_status_code(response5, 200)  # Сдесь явный баг!!!
+
+        response6 = MyRequests.get(f"/user/{user_id1}")
+        Assertions.assert_status_code(response6, 200)
+
